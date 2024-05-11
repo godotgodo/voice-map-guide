028b2cba888e93164837476f1f1b51be910c3d19
godotgodo <yunusemregolbasi0@gmail.com>
2024-05-11 14:41:07 +0300

    While loop added.

--- app/src/main/java/com/example/yazlab/MainActivity.kt
+++ app/src/main/java/com/example/yazlab/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -34,98 +35,116 @@ class MainActivity : ComponentActivity() {
         val myButton: Button = findViewById(R.id.myButton)
         myButton.setOnClickListener {
             lifecycleScope.launch(Dispatchers.IO) {
-                val editText = findViewById<EditText>(R.id.edText)
-                //val (latitude, longitude) = editText.text.toString().split(",").map { it.trim().toDoubleOrNull() ?: return@map null }
-                //edittextten alabiliriz şu şekilde şimdilik böyle bırakıyorum
-                val (latitude, longitude) = "40.9880, 29.0378".split(",").map { it.trim().toDoubleOrNull() ?: return@map null }
-                val apiKey = "api_key"
-                val mode = "walking"
+                while(true) {
+                    val editText = findViewById<EditText>(R.id.edText)
+                    //val (latitude, longitude) = editText.text.toString().split(",").map { it.trim().toDoubleOrNull() ?: return@map null }
+                    //edittextten alabiliriz şu şekilde şimdilik böyle bırakıyorum
+                    val (latitude, longitude) = "40.9880, 29.0378".split(",")
+                        .map { it.trim().toDoubleOrNull() ?: return@map null }
+                    val apiKey = "key"
+                    val mode = "walking"
 
-                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
-                if (ActivityCompat.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    return@launch
-                }
-                fusedLocationClient.lastLocation
-                    .addOnSuccessListener { location ->
-                        location?.let {
-                            val userLatitude = it.latitude
-                            val userLongitude = it.longitude
-                            val currentLocation = "$userLatitude,$userLongitude"
-                            val destination = "37.9880, -120.00000"
-                            //şimdilik statik, edText'ten gelen alınacak yukarıda açıklamıştım
-                            //val destination = "$latitude,$longitude"
-                            val url = "https://maps.googleapis.com/maps/api/directions/json?" +
-                                    "origin=$currentLocation" +
-                                    "&destination=$destination" +
-                                    "&mode=$mode" +
-                                    "&key=$apiKey"
+                    val fusedLocationClient =
+                        LocationServices.getFusedLocationProviderClient(this@MainActivity)
+                    if (ActivityCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        return@launch
+                    }
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location ->
+                            location?.let {
+                                val userLatitude = it.latitude
+                                val userLongitude = it.longitude
+                                val currentLocation = "$userLatitude,$userLongitude"
+                                val destination = "37.9880, -120.00000"
+                                //şimdilik statik, edText'ten gelen alınacak yukarıda açıklamıştım
+                                //val destination = "$latitude,$longitude"
+                                val url = "https://maps.googleapis.com/maps/api/directions/json?" +
+                                        "origin=$currentLocation" +
+                                        "&destination=$destination" +
+                                        "&mode=$mode" +
+                                        "&key=$apiKey"
 
-                            val client = OkHttpClient()
-                            val request = Request.Builder()
-                                .url(url)
-                                .build()
-                            try {
-                                client.newCall(request).execute().use { response ->
-                                    if (!response.isSuccessful) throw IOException("Failed to get directions: $response")
-                                    val jsonResponse = JSONObject(response.body!!.string())
-                                    val routes = jsonResponse.getJSONArray("routes")
-                                    if (routes.length() > 0) {
-                                        val firstRoute = routes.getJSONObject(0)
-                                        val legs = firstRoute.getJSONArray("legs")
-                                        val firstLeg = legs.getJSONObject(0)
-                                        val steps = firstLeg.getJSONArray("steps")
-                                        if (steps.length() > 0) {
-                                            val firstStep = steps.getJSONObject(0)
-                                            val distance = firstStep.getJSONObject("distance")
-                                            val meters = distance.get("value")
-                                            try {
-                                                val maneuver = firstStep.get("maneuver")
-                                                val instruction=(meters.toString() + " metre sonra " + maneuver.toString())
-                                                tts = TextToSpeech(applicationContext) { status ->
-                                                    if (status == TextToSpeech.SUCCESS) {
-                                                        tts.language = Locale.US
-                                                        tts.setSpeechRate(1.0f)
-                                                        tts.speak(instruction, TextToSpeech.QUEUE_ADD, null)
-                                                    }
-                                                }
-                                            } catch (e: Exception) {
-                                                if (e is org.json.JSONException) {
-                                                    val instruction=(meters.toString() + " metre boyunca devam edin")
-                                                    println(instruction)
-                                                    tts = TextToSpeech(applicationContext) { status ->
-                                                        if (status == TextToSpeech.SUCCESS) {
-                                                            tts.language = Locale.US
-                                                            tts.setSpeechRate(1.0f)
-                                                            tts.speak(instruction, TextToSpeech.QUEUE_ADD, null)
+                                val client = OkHttpClient()
+                                val request = Request.Builder()
+                                    .url(url)
+                                    .build()
+                                try {
+                                    client.newCall(request).execute().use { response ->
+                                        if (!response.isSuccessful) throw IOException("Failed to get directions: $response")
+                                        val jsonResponse = JSONObject(response.body!!.string())
+                                        val routes = jsonResponse.getJSONArray("routes")
+                                        if (routes.length() > 0) {
+                                            val firstRoute = routes.getJSONObject(0)
+                                            val legs = firstRoute.getJSONArray("legs")
+                                            val firstLeg = legs.getJSONObject(0)
+                                            val steps = firstLeg.getJSONArray("steps")
+                                            if (steps.length() > 0) {
+                                                val firstStep = steps.getJSONObject(0)
+                                                val distance = firstStep.getJSONObject("distance")
+                                                val meters = distance.get("value")
+                                                try {
+                                                    val maneuver = firstStep.get("maneuver")
+                                                    val instruction =
+                                                        (meters.toString() + " metre sonra " + maneuver.toString())
+                                                    print(instruction)
+                                                    tts =
+                                                        TextToSpeech(applicationContext) { status ->
+                                                            if (status == TextToSpeech.SUCCESS) {
+                                                                tts.language = Locale.US
+                                                                tts.setSpeechRate(1.0f)
+                                                                tts.speak(
+                                                                    instruction,
+                                                                    TextToSpeech.QUEUE_ADD,
+                                                                    null
+                                                                )
+                                                            }
                                                         }
+                                                } catch (e: Exception) {
+                                                    if (e is org.json.JSONException) {
+                                                        val instruction =
+                                                            (meters.toString() + " metre boyunca devam edin")
+                                                        println(instruction)
+                                                        tts =
+                                                            TextToSpeech(applicationContext) { status ->
+                                                                if (status == TextToSpeech.SUCCESS) {
+                                                                    tts.language = Locale.US
+                                                                    tts.setSpeechRate(1.0f)
+                                                                    tts.speak(
+                                                                        instruction,
+                                                                        TextToSpeech.QUEUE_ADD,
+                                                                        null
+                                                                    )
+                                                                }
+                                                            }
                                                     }
                                                 }
+                                            } else {
+                                                println("Adım bulunamadı")
                                             }
-                                        } else {
-                                            println("Adım bulunamadı")
                                         }
                                     }
+                                } catch (e: IOException) {
+                                    println("IOException: ${e.message}")
+                                } catch (e: JSONException) {
+                                    println("JSONException: ${e.message}")
+                                } catch (e: Exception) {
+                                    println("Exception: $e")
                                 }
-                            } catch (e: IOException) {
-                                println("IOException: ${e.message}")
-                            } catch (e: JSONException) {
-                                println("JSONException: ${e.message}")
-                            } catch (e: Exception) {
-                                println("Exception: $e")
                             }
                         }
-                    }
-                    .addOnFailureListener { exception ->
-                        // Konum alınamadığında yapılacak işlemler
-                        println("Konum alınamadı: ${exception.message}")
-                    }
+                        .addOnFailureListener { exception ->
+                            // Konum alınamadığında yapılacak işlemler
+                            println("Konum alınamadı: ${exception.message}")
+                        }
+                    delay(10000)
+                }
             }
         }
     }
